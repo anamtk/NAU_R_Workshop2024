6d766cf0e6944a9493ac02a23cf394597928e461
Ana Miller-ter Kuile <ana.miller.ter.kuile@gmail.com>
2025-01-06 09:29:46 -0700

    added some resources so we can hyperlink them

---
+++ livecoding/01_gettingstarted.R
@@ -0,0 +1,37 @@
+#Getting started with R and R Studio
+#Ana Miller-ter Kuile
+#12/17/2024
+
+#this script introduces how to use R and R Studio
+#and also installs some important packages
+
+
+# Simple math -------------------------------------------------------------
+
+
+
+1+1
+
+#note to future me
+
+2+5
+6-3
+
+
+# Introduction to functions -----------------------------------------------
+
+
+#use the sum() function to add numbers
+sum(1,1)
+
+
+# Install packages --------------------------------------------------------
+
+
+#install packages for next sessions
+#install.packages(c("tidyverse", "lubridate", "ratdat"))
+library(tidyverse)
+library(lubridate)
+library(ratdat)
+
+
