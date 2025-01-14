149c2aee5301450bf171616c1d7b01d32a343082
Ana Miller-ter Kuile <ana.miller.ter.kuile@gmail.com>
2025-01-14 14:50:13 -0700

    updating scripts and added link to recordings

--- livecoding/02_plotsinggplot2.R
+++ livecoding/02_plotsinggplot2.R
@@ -74,3 +74,72 @@ ggplot(data = complete_old, mapping = aes(x = weight, y = hindfoot_length, color
   geom_point(alpha = 0.2) +
   scale_color_viridis_d() +
   theme_bw()
+
+# Boxplots! ---------------------------------------------------------------
+
+#let's look at differences in plot types with a boxplot
+ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length)) +
+  geom_boxplot()
+
+#add color to my boxplot
+ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length, color = plot_type)) +
+  geom_boxplot()
+
+#rather than changing the outlines to be colored, let's "fill"
+#our boxplots by plot type
+ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length, fill = plot_type)) +
+  geom_boxplot()
+
+
+# Adding geoms together ---------------------------------------------------
+
+ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length)) +
+  geom_boxplot() +
+  geom_point(alpha = 0.2)
+
+#add jitter to points so we can see them better 
+ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length)) +
+  geom_boxplot() +
+  geom_jitter(alpha = 0.2)
+
+#add color to both the points and boxplots
+ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length, color = plot_type)) +
+  geom_boxplot() +
+  geom_jitter(alpha = 0.2)
+
+#adding color to only the points
+ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length)) +
+  geom_boxplot() +
+  geom_jitter(mapping = aes(color = plot_type), alpha = 0.2)
+
+
+# Assigning objects -------------------------------------------------------
+
+#create an object that is my plot
+myplot <- ggplot(data = complete_old, mapping = aes(x = plot_type, y = hindfoot_length)) +
+  geom_boxplot() +
+  geom_jitter(mapping = aes(color = plot_type), alpha = 0.2)
+
+myplot
+
+#add things to our myplot object
+myplot + 
+  theme_bw()
+
+#change axis labels
+myplot +
+  theme_bw() +
+  theme(axis.title = element_text(size = 14)) +
+  labs(title = "Rodent sizes by plot type", x = "Plot type", y = "Hindfoot length (mm)")
+
+
+# Exporting plots ---------------------------------------------------------
+
+#create another object that is the final plot to save
+finalplot <-  myplot +
+  theme_bw() +
+  theme(axis.title = element_text(size = 14)) +
+  labs(title = "Rodent sizes by plot type", x = "Plot type", y = "Hindfoot length (mm)")
+
+#use the ggsave function to save this plot on our computer
+ggsave(filename = 'images/rodent_size_plot.jpg', plot = finalplot, height = 6, width = 8)
